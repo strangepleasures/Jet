e47ccf46ca7d1edee7c6cc9e0750fb114a9d14e9
strangepleasures <pavel@thehyve.nl>
2020-12-07 18:42:25 +0100

    A better LazilyMappedRange.toString

--- src/main/kotlin/com/jetbrains/jet/engine/LazilyMappedRange.kt
+++ src/main/kotlin/com/jetbrains/jet/engine/LazilyMappedRange.kt
@@ -1,35 +1,50 @@
 package com.jetbrains.jet.engine
 
 import java.util.stream.LongStream
+import java.util.stream.Stream
 
-class LazilyMappedRange<T>(private val startInclusive: Long, private val endInclusive: Long, private val mapper: (Long) -> T) {
+class LazilyMappedRange<T>(
+    private val startInclusive: Long,
+    private val endInclusive: Long,
+    private val mapper: (Long) -> T
+) {
     companion object {
         fun of(startInclusive: Long, endInclusive: Long) = LazilyMappedRange(startInclusive, endInclusive) { it }
     }
 
     fun <R> map(f: (T) -> R) = LazilyMappedRange(startInclusive, endInclusive) { f(mapper(it)) }
 
-    fun stream() = LongStream.rangeClosed(startInclusive, endInclusive).mapToObj(mapper)
+    fun stream(): Stream<T> = LongStream.rangeClosed(startInclusive, endInclusive).mapToObj(mapper)
 
-    override fun toString(): String  {
-        val builder = StringBuilder("[")
+    fun dump(builder: StringBuilder) {
+        builder.append('[')
         stream().takeWhile {
-            if (builder.length > 1) {
+            if (builder.length > 1000) {
+                builder.append("...")
+                return@takeWhile false
+            }
+
+            if (builder[builder.length - 1] != '[') {
                 builder.append(", ")
             }
-            if (it is Double && it.compareTo(it.toLong()) == 0) {
-                builder.append(it.toLong()) // compact if possible
+
+            if (it is LazilyMappedRange<*>) {
+                it.dump(builder)
+            } else if (it is Double && it.compareTo(it.toLong()) == 0) {
+                builder.append((it as Double).toLong())
             } else {
                 builder.append(it)
             }
-            if (builder.length > 1000) {
-                builder.append("...")
-                false
-            } else {
-                true
-            }
+
+            return@takeWhile true
         }.forEach { /* consume */ }
 
-        return builder.append("]").toString()
+        builder.append("]")
+    }
+
+    override fun toString(): String {
+        val builder = StringBuilder()
+        dump(builder)
+        return builder.toString()
     }
 }
